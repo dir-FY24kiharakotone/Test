abeb7d87911c8a63c1454ee59979f44221d318a3
KotoneKihara(木原琴音) <kotone.kihara.a426@dirbato.co.jp>
2024-04-11 15:59:38 +0900

    最初のコミット

---
+++ Test/src/calc/CalculatorTest.java
@@ -0,0 +1,20 @@
+package calc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    @Test
+    void testAdd() {
+        Calculator calculator = new Calculator(); //クラス名　変数
+        assertEquals(5, calculator.add(2, 3));
+            }
+
+    @Test
+    void testSubstract() {
+        Calculator calculator = new Calculator();
+        assertEquals(-1, calculator.substract(2, 3));
+            }
+}
